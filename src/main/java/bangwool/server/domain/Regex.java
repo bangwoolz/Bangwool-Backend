3fe7d46f39998a6fccbd060ed5b01a14eebbd6f1
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-07-08 17:55:19 +0900

    fix: 닉네임 정규 표현식 수정

--- src/main/java/bangwool/server/domain/Regex.java
+++ src/main/java/bangwool/server/domain/Regex.java
@@ -4,5 +4,5 @@ public class Regex {
 
     static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
     static final String PASSWORD_REGEX = "^(?=.*[a-zA-Z0-9])(?=.*[!@#$%^&*()](?=\\S+$).{8.12}$";
-    static final String NICKNAME_REGEX = "^[A-Za-z0-9가-힣]*$.{0,10}";
+    static final String NICKNAME_REGEX = "^[a-zA-Z0-9가-힣]{1,5}$";
 }
