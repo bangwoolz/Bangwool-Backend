6517df314005408a5ad7f7ada9459696a1712153
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-07-09 13:01:45 +0900

    feat: 멤버 테스트 파일 작성

--- src/main/java/bangwool/server/domain/Regex.java
+++ src/main/java/bangwool/server/domain/Regex.java
@@ -2,7 +2,7 @@ package bangwool.server.domain;
 
 public class Regex {
 
-    static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
-    static final String PASSWORD_REGEX = "^(?=.*[a-zA-Z0-9])(?=.*[!@#$%^&*()](?=\\S+$).{8.12}$";
-    static final String NICKNAME_REGEX = "^[a-zA-Z0-9가-힣]{1,5}$";
+    public static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+    public static final String PASSWORD_REGEX = "^(?=.*[a-zA-Z0-9])(?=.*[!@#$%^&*()])(?=\\S+$).{8,12}$";
+    public static final String NICKNAME_REGEX = "^[a-zA-Z0-9가-힣]{1,5}$";
 }
---
+++ src/main/java/bangwool/server/exception/RegexException.java
@@ -0,0 +1,10 @@
+package bangwool.server.exception;
+
+public class RegexException {
+
+    public static final String EMAIL_EXCEPTION = "이메일 형식을 확인해 주세요";
+    public static final String NIKCNAME_EXCEPTION = "닉네임 형식을 확인해 주세요";
+    public static final String PASSWORD_EXCEPTION = "비밀번호 형식을 확인해 주세요";
+
+    public static final String EMPTY_EXCEPTION = "";
+}
