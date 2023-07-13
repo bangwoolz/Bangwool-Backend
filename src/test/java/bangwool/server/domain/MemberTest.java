8e4a931416044427b1da572ebc311811d96be354
Jungwoo Kim <76640167+jung-woo-kim@users.noreply.github.com>
2023-07-13 21:39:39 +0900

    feat: 멤버 클래스, 테이블 구현 (#2) (#3)
    * docs: todoList 작성
    
    * feat: Member 필드 정규 표현식 상수로 표현
    
    * fix: TODO 파일 수정
    
    * fix: 닉네임 정규 표현식 수정
    
    * feat: 멤버 테스트 파일 작성
    
    * feat: Lombok, hibernate.validator 추가
    
    * chore: 환경 변수 설정
    
    * feat: Member 클래스 구현
    
    * fix: 코드 리뷰 반영
    
    - Regex in Member
    - local h2로 변경
    - ddl-auto local,dev,prod에 맞게 설정
    - eol추가
    - TODO 파일 삭제
    
    Co-authored-by: HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>

---
+++ src/test/java/bangwool/server/domain/MemberTest.java
@@ -0,0 +1,121 @@
+package bangwool.server.domain;
+
+import bangwool.server.exception.RegexException;
+import jakarta.validation.ConstraintViolation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import jakarta.validation.Validator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import static bangwool.server.domain.MemberTest.Type.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WebMvcTest
+@DisplayName("멤버 테스트")
+class MemberTest {
+
+    enum Type{
+        EMAIL, PASSWORD, NICKNAME
+    }
+
+    @Autowired
+    private Validator validator;
+
+    @Test
+    @DisplayName("이메일 정규 표현식이 적용이 되는지?")
+    void valid_correct_email() {
+        // given
+        String[] corrects = {"ttt@gmail.com", "ttt123@gmail.co.kr", "123@gmail.com"};
+        String[] incorrects = {"@gmail.com", "ttt", "ttt@gmailcom"};
+
+        //when
+        validRegex(corrects, RegexException.EMPTY_EXCEPTION, EMAIL);
+        validRegex(incorrects, RegexException.EMAIL_EXCEPTION, EMAIL);
+
+        //then
+
+    }
+
+    @Test
+    @DisplayName("닉네임 글자 제한이 적용 되는지?")
+    void valid_correct_nickname() {
+        //given
+        String[] corrects = {"아", "야야야야야", "야호", "test", "t", "2", "2222"};
+        String[] incorrects = {"", "ㅇㅇ", "21글자채우기용으로뭐라도적어보는중입니다아", "rmfwkcodnrldyddlqslek"};
+
+        //when
+        validRegex(corrects , RegexException.EMPTY_EXCEPTION, NICKNAME);
+        validRegex(incorrects , RegexException.NIKCNAME_EXCEPTION, NICKNAME);
+
+        //then
+
+    }
+
+    @Test
+    @DisplayName("비밀번호가 암호화가 되어있는지?")
+    void encoding_password() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    @DisplayName("비밀번호가 정규 표현식이 적용 되는지?")
+    void valid_correct_password() {
+        //given
+        String[] corrects = {"Password!", "password!", "!!!PP123", "!!!pp123"};
+        String[] incorrects = {"PPPPPPP", "!!!!!!!!", "password1", "!!!p123",
+                "p", "PPPassword", "PPPaaassssswoorld!!"};
+
+        //when
+        validRegex(corrects , RegexException.EMPTY_EXCEPTION, PASSWORD);
+        validRegex(incorrects, RegexException.PASSWORD_EXCEPTION, PASSWORD);
+
+        //then
+
+
+    }
+
+    private void validRegex(String[] tests, final String expected, Type type) {
+
+        //given
+        List<String> messages = new ArrayList<>();
+        Member member;
+
+        //when
+        for(String test : tests) {
+            switch (type) {
+                case EMAIL -> member = Member.builder().email(test).build();
+                case NICKNAME -> member = Member.builder().nickname(test).build();
+                case PASSWORD -> member = Member.builder().password(test).build();
+                default -> member = Member.builder().build();
+            }
+
+            Set<ConstraintViolation<Member>> validate = validator.validate(member);
+
+            Iterator<ConstraintViolation<Member>> iterator = validate.iterator();
+
+            iterator.forEachRemaining(iter -> messages.add(iter.getMessage()));
+        }
+
+        //then
+        if(expected.equals(RegexException.EMPTY_EXCEPTION)) {
+            assertThat(messages).isEmpty();
+        } else {
+            assertEquals(tests.length, messages.size());
+            for(String message : messages) {
+                assertEquals(message, expected);
+            }
+        }
+    }
+
+}
