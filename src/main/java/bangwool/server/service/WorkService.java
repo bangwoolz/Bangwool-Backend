8fe659c8790d4d8722de6ef01c19d4e5fda36c3c
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-08-05 15:20:08 +0900

    feat: WorkService 월별 작업 반환 서비스 구현

--- src/main/java/bangwool/server/service/WorkService.java
+++ src/main/java/bangwool/server/service/WorkService.java
@@ -1,14 +1,14 @@
 package bangwool.server.service;
 
-import bangwool.server.domain.Member;
 import bangwool.server.domain.Ppomodoro;
 import bangwool.server.domain.Work;
 import bangwool.server.dto.request.WorkRequest;
 import bangwool.server.dto.response.WorkResponse;
 import bangwool.server.dto.response.WorkTodayResponse;
+import bangwool.server.dto.response.WorksMonthResponse;
 import bangwool.server.dto.response.WorksTodayResponse;
 import bangwool.server.exception.notfound.NotFoundMemberException;
-import bangwool.server.exception.notfound.NotFoundPpomodororException;
+import bangwool.server.exception.notfound.NotFoundPpomodoroException;
 import bangwool.server.repository.MemberRepository;
 import bangwool.server.repository.PpomodoroRepository;
 import bangwool.server.repository.WorkRepository;
@@ -17,7 +17,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -34,7 +36,7 @@ public class WorkService {
         memberRepository.findById(memberId)
                 .orElseThrow(NotFoundMemberException::new);
         Ppomodoro ppomodoro = ppomodoroRepository.findById(ppomodoroId)
-                .orElseThrow(NotFoundPpomodororException::new);
+                .orElseThrow(NotFoundPpomodoroException::new);
         Work work = workRepository.save(Work.builder()
                 .workedHour(request.getWorkedHour())
                 .workedMin(request.getWorkedMin())
@@ -45,13 +47,24 @@ public class WorkService {
         return new WorkResponse(work.getId());
     }
 
-    //todo r : 오늘의 뽀모도로 어떻게 했는지? -> 통계자료 만들어서, 랭킹
-
     public WorksTodayResponse findTodayPpomodoro(Long memberId) {
        memberRepository.findById(memberId)
                .orElseThrow(NotFoundMemberException::new);
         List<WorkTodayResponse> todayWorkedPpomodoro = workRepository
                 .findTodayWorkByMemberId(memberId, Date.valueOf(LocalDate.now()));
+
         return new WorksTodayResponse(todayWorkedPpomodoro);
     }
+
+    public WorksMonthResponse findMonthPpomodoro(Long memberId,int year, int month) {
+        memberRepository.findById(memberId)
+                .orElseThrow(NotFoundMemberException::new);
+
+        Timestamp baseMonth = Timestamp.valueOf(LocalDateTime.of(year,month,1,6,0));
+        Timestamp endMonth = Timestamp.valueOf(LocalDateTime.of(year, month+1, 1, 6, 0));
+
+        return new WorksMonthResponse(workRepository.findMonthWorkByMemberId(memberId, baseMonth, endMonth));
+    }
+
+
 }
